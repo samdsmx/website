42157f8e4702e035b569fa0dacbe9bd682e63991
Daniel Aguilera González <twinber@hotmail.com>
2018-08-28 12:07:31 +0100

    actualizacion de firebase-tools en travis.sh (#278)
    problemas deploy con firebase

--- tool/travis.sh
+++ tool/travis.sh
@@ -80,7 +80,7 @@ rake checklinks
 if [ "$TRAVIS_EVENT_TYPE" = "push" ] && [ "$TRAVIS_BRANCH" = "master" ]; then
   # Deploy pushes to master to Firebase hosting.
   echo "Deploying to Firebase."
-  npm install --global firebase-tools@3.19.3
+  npm install --global firebase-tools@4.2.0
   firebase -P flutter-es --token "$FIREBASE_TOKEN" --non-interactive deploy
 fi
 
