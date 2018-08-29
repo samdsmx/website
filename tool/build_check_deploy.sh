74e3ebcb55d755237d4907ee82117befaf1d70ab
Daniel Aguilera González <twinber@hotmail.com>
2018-08-29 20:36:00 +0100

    Bug deploy 2 (#288)
    * deploy dentro de build_check_deploy
    
    * vuelvo a una sola ejecución para deploy y checks

--- tool/build_check_deploy.sh
+++ tool/build_check_deploy.sh
@@ -159,6 +159,13 @@ else
   echo "SKIPPING: check links"
 fi
 
+if [ "$TRAVIS_EVENT_TYPE" = "push" ] && [ "$TRAVIS_BRANCH" = "master" ]; then
+  # Deploy pushes to master to Firebase hosting.
+  echo "Deploying to Firebase."
 
+  npm install --global firebase-tools
+  firebase -P flutter-es --token "$FIREBASE_TOKEN" --non-interactive deploy
+
+fi
 
 exit 0
